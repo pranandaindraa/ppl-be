d9afb0d5d748443cdc287fe6b72308e5d2125d86
pranandaindraa <indraprananda29agmail.com>
2024-11-14 21:53:32 +0800

    update port

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ app.get("/ipk/:nim", (req, res) => {
 });
 
 // Jalankan server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
